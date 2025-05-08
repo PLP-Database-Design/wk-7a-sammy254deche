c667de8363d355a328ac416e285b564b1d5dfbac
sammy254deche <nduchisammy@gmail.com>
2025-05-08 12:59:47 +0300

    Update answers.sql

--- answers.sql
+++ answers.sql
@@ -25,3 +25,21 @@ VALUES
     (101, 'John Doe'),
     (102, 'Jane Smith'),
     (103, 'Emily Clark');
+
+
+CREATE TABLE OrderDetails_2NF (
+    OrderID INT,
+    Product VARCHAR(255),
+    Quantity INT,
+    PRIMARY KEY (OrderID, Product),
+    FOREIGN KEY (OrderID) REFERENCES Customers(OrderID)
+);
+
+INSERT INTO OrderDetails_2NF (OrderID, Product, Quantity)
+VALUES 
+    (101, 'Laptop', 2),
+    (101, 'Mouse', 1),
+    (102, 'Tablet', 3),
+    (102, 'Keyboard', 1),
+    (102, 'Mouse', 2),
+    (103, 'Phone', 1);
